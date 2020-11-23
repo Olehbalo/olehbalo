f9b4287f9251676c03c0dc07850b5c0d175d7ce8
Olehbalo <balo.oleh.p@gmail.com>
2020-11-23 11:50:44 +0200

    my first commit

---
+++ Source.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+char String[] = { 0 };
+bool test(char* String)
+{
+    int x = 0, o = 0, i = 0;
+    while (String[i] != '\0')
+    {
+        if (String[i] == 'o') {
+            o = o + 1;
+        }
+        if (String[i] == 'x') {
+            x = x + 1;
+        }
+        i++;
+    }
+    if (o == x) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+int main() {
+
+    printf("String: ");
+    scanf("%s", String);
+    printf("%d\n", test(String));
+
+    return 0;
+
+}
